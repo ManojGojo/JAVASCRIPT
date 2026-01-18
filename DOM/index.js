38fd7f44208bd59904de6960d9fb9f3e2bcf128d
ManojGojo <deshmukhmanoj913@gmail.com>
2026-01-18 12:12:34 +0530

    DOM IN JS

---
+++ DOM/index.js
@@ -0,0 +1,54 @@
+// function Hello(Name){
+//     console.log(`Hello ${Name} How are you?`);
+// }
+
+// let result=Hello("Manoj");
+// console.log(result);
+
+// // let clickButton=document.getElementById("myButton");
+// // console.log(clickButton);
+
+// // let clickButton=document.getElementsByClassName("button");
+// // console.log(clickButton);
+
+// let clickButton=document.querySelector(".button");
+// console.log(clickButton);
+
+// function Hello(){
+//     console.log("Button Clicked!!");
+// }
+
+// let clickButton=document.querySelector(".button1");
+// clickButton.addEventListener("click",()=>{
+//     clickButton.innerText="PRESS ME!!"
+// });
+
+// let clickButton=document.querySelector(".button1");
+// clickButton.addEventListener("click",()=>{
+// clickButton.classList.add("btnColor");
+// })
+
+// let clickButton=document.querySelector(".button1");
+// clickButton.addEventListener("click",()=>{
+// document.body.classList.toggle("bgColor");
+// })
+
+// let input=document.querySelector(".namaste")
+// input.addEventListener("focus",()=>{
+//     console.log(input.value);
+// })
+
+// let input=document.querySelector(".namaste")
+// input.addEventListener("change",()=>{
+//     console.log(input.value);
+// })
+
+// let input=document.querySelector(".namaste")
+// input.addEventListener("input",()=>{
+//     console.log(input.value);
+// })
+
+// let input=document.querySelector(".namaste")
+// input.addEventListener("keyup",()=>{
+//     console.log(input.value);
+// })
---
+++ DOM/index2.js
@@ -0,0 +1,37 @@
+console.log("index2.js loaded");
+
+//let input=document.querySelector(".namaste");
+
+// input.addEventListener("mousedown", (event) => {
+//   console.log(event.button);
+// });
+
+  
+// input.addEventListener("mousedown",(event)=>{
+//     let xcor=event.pageX;
+//     let ycor=event.pageY;
+//     let cors=`X cor -${xcor}, Y cor -${ycor}`;
+//     console.log(cors);
+// })
+
+
+// document.body.addEventListener("mousedown",(event)=>{
+//     let xcor=event.pageX;
+//     let ycor=event.pageY;
+//     let cors=`X cor -${xcor}, Y cor -${ycor}`;
+//     console.log(cors);
+// })
+
+//Deligation
+
+
+// let clickButton=document.querySelector(".button-container");
+// clickButton.addEventListener("click",(karyakram)=>{
+//     btnText=karyakram.target.innerText;
+//     if(btnText=="Button 1"){
+//         karyakram.target.classList.add("btnColor")
+//     }
+//       else if(btnText="Button 2"){
+//         karyakram.target.classList.add("btnColor2")
+//        }
+// })
